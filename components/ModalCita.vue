09359ae8606c84fdf59d815c0e3fde0f12236220
Ángel Vásquez <angel.vasquez.unam@gmail.com>
2018-05-30 16:17:09 -0500

    Se agregan links para llamar y enviar correo

--- components/ModalCita.vue
+++ components/ModalCita.vue
@@ -1,19 +1,52 @@
 <template lang="pug">
   .modal(:class="{'is-active': active}")
-    .modal-background
-    .modal-card.is-success
+    .modal-background(@click="close")
+    .modal-card
       header.modal-card-head
         p.modal-card-title Agendar Cita
         button.delete(aria-label="close", @click="close")
       section.modal-card-body
-        h1.title Hola
-        p Puede llamar al número: 55-55555555
-        p Mandarnos un correo a: citas@tuli.com
+        h1.title ¡Excelente decisión!
+        div Puede llamar al número: &nbsp;
+          a.button.is-psico1(href="tel:+5591889299")
+            .media
+              .media-left
+                p.image.is-24x24
+                  img(src="~/assets/phone.png", alt="Phone")
+              .media-content
+                .content
+                  p +5591889299
+        br
+
+        div Mandarnos un correo a: &nbsp;
+          a.button(href="mailto:citas@tuli.com", target="_blank")
+            .media
+              .media-left
+                p.image.is-24x24
+                  img(src="~/assets/mail.png", alt="Mail")
+              .media-content
+                .content
+                  p citas@tuli.com
+        
         hr
         p Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet inventore veniam quas dolores nulla facere necessitatibus vitae quasi nihil, voluptas placeat voluptates natus hic dolorem quia ea tenetur odio unde?
       footer.modal-card-foot
-        button.button.is-success Save changes
-        button.button(@click="close") Cancel
+        a.button.is-psico1(href="tel:+5591889299")
+          .media
+            .media-left
+              p.image.is-24x24
+                img(src="~/assets/phone.png", alt="Phone")
+            .media-content
+              .content
+                p Llamar ahora
+        a.button(href="mailto:citas@tuli.com", target="_blank")
+          .media
+            .media-left
+              p.image.is-24x24
+                img(src="~/assets/mail.png", alt="Mail")
+            .media-content
+              .content
+                p Enviar correo
 </template>
 
 <script>
